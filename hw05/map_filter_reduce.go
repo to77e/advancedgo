89273aa14f4cdcece34b02889b54dbd39fd15e79
to77e <skzntcv@gmail.com>
2025-04-27 13:35:13 +0200

    homework#05: map, filter, reduce

---
+++ hw05/map_filter_reduce.go
@@ -0,0 +1,37 @@
+package hw05
+
+func Map(data []int, action func(int) int) []int {
+	if data == nil {
+		return nil
+	}
+	newData := make([]int, 0, len(data))
+	for _, v := range data {
+		newData = append(newData, action(v))
+	}
+	return newData
+}
+
+func Filter(data []int, action func(int) bool) []int {
+	if data == nil {
+		return nil
+	}
+	newData := make([]int, 0, len(data))
+	for _, v := range data {
+		if ok := action(v); !ok {
+			continue
+		}
+		newData = append(newData, v)
+	}
+	return newData
+}
+
+func Reduce(data []int, initial int, action func(int, int) int) int {
+	if data == nil {
+		return 0
+	}
+	result := initial
+	for _, v := range data {
+		result = action(v, result)
+	}
+	return result
+}
