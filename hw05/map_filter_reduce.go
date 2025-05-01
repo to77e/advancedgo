ad93d41eed2ad87d6473bf4b35ac33d25a1efc87
to77e <skzntcv@gmail.com>
2025-05-01 11:12:19 +0200

    fix: some remarks

--- hw05/map_filter_reduce.go
+++ hw05/map_filter_reduce.go
@@ -4,6 +4,9 @@ func Map(data []int, action func(int) int) []int {
 	if data == nil {
 		return nil
 	}
+	if len(data) == 0 {
+		return []int{}
+	}
 	newData := make([]int, 0, len(data))
 	for _, v := range data {
 		newData = append(newData, action(v))
@@ -14,6 +17,9 @@ func Map(data []int, action func(int) int) []int {
 func Filter(data []int, action func(int) bool) []int {
 	if data == nil {
 		return nil
+	}
+	if len(data) == 0 {
+		return []int{}
 	}
 	newData := make([]int, 0, len(data))
 	for _, v := range data {
