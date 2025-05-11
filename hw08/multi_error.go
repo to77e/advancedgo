c0f27e796795844629f2504f07bb0d34855c9a4c
to77e <skzntcv@gmail.com>
2025-05-11 22:19:41 +0200

    homework#08: multi error

---
+++ hw08/multi_error.go
@@ -0,0 +1,39 @@
+package hw08
+
+import (
+	"fmt"
+	"strings"
+)
+
+type MultiError struct {
+	errors []error
+}
+
+func (e *MultiError) Error() string {
+
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("%d errors occurred:\n", len(e.errors)))
+
+	for _, err := range e.errors {
+		sb.WriteString(fmt.Sprintf("\t* %s", err.Error()))
+	}
+
+	sb.WriteString("\n")
+	return sb.String()
+}
+
+func Append(err error, errs ...error) *MultiError {
+	multiError := &MultiError{}
+
+	if err != nil {
+		if me, ok := err.(*MultiError); ok {
+			multiError.errors = append(multiError.errors, me.errors...)
+		} else {
+			multiError.errors = append(multiError.errors, err)
+		}
+	}
+
+	multiError.errors = append(multiError.errors, errs...)
+
+	return multiError
+}
