310942ea73b75e75c6b07e7bae38381e33fcc3bb
to77e <skzntcv@gmail.com>
2025-05-11 17:30:09 +0200

    homework#07: di container

---
+++ hw07/di_container.go
@@ -0,0 +1,35 @@
+package hw07
+
+import "errors"
+
+type UserService struct {
+	// not need to implement
+	NotEmptyStruct bool
+}
+type MessageService struct {
+	// not need to implement
+	NotEmptyStruct bool
+}
+
+type Container struct {
+	providers map[string]interface{}
+}
+
+func NewContainer() *Container {
+	return &Container{
+		providers: make(map[string]interface{}),
+	}
+}
+
+func (c *Container) RegisterType(name string, constructor interface{}) {
+	c.providers[name] = constructor
+}
+
+func (c *Container) Resolve(name string) (interface{}, error) {
+	constructor, ok := c.providers[name]
+	if !ok {
+		return nil, errors.New("service not found")
+	}
+
+	return constructor.(func() interface{})(), nil
+}
