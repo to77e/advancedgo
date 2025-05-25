93ab96788cd67976a0194ca6fb9d9ac133b834df
to77e <skzntcv@gmail.com>
2025-05-25 15:44:25 +0200

    homework#09: person

---
+++ hw09/person.go
@@ -0,0 +1,42 @@
+package hw09
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+type Person struct {
+	Name    string `properties:"name"`
+	Address string `properties:"address,omitempty"`
+	Age     int    `properties:"age"`
+	Married bool   `properties:"married"`
+}
+
+type User struct {
+	Id     int     `properties:"id"`
+	Login  string  `properties:"login"`
+	Name   string  `properties:"name,omitempty"`
+	Email  string  `properties:"email,omitempty"`
+	Age    int     `properties:"age,omitempty"`
+	Active bool    `properties:"active"`
+	Score  float32 `properties:"score"`
+}
+
+func Serialize(person interface{}) string {
+	var result []string
+	t := reflect.TypeOf(person)
+	v := reflect.ValueOf(person)
+
+	for i := 0; i < t.NumField(); i++ {
+		tFieldTag := t.Field(i).Tag
+		value, present := tFieldTag.Lookup("properties")
+		if !present || (strings.Contains(value, "omitempty") && v.Field(i).IsZero()) {
+			continue
+		}
+		name := strings.Split(value, ",")[0]
+		vField := v.Field(i)
+		result = append(result, fmt.Sprintf("%s=%v", name, vField))
+	}
+	return strings.Join(result, "\n")
+}
