7a838df22e7ac265237f993aaf144808ea921876
to77e <skzntcv@gmail.com>
2025-06-01 08:11:45 +0200

    homework#11: trace

---
+++ hw11/trace.go
@@ -0,0 +1,42 @@
+package hw11
+
+import "unsafe"
+
+func Trace(stacks [][]uintptr) []uintptr {
+	seen := make(map[uintptr]bool)
+	var roots []uintptr
+
+	for _, stack := range stacks {
+		for _, u := range stack {
+			if u == 0 {
+				continue
+			}
+			if !seen[u] {
+				roots = append(roots, u)
+				seen[u] = true
+			}
+		}
+	}
+
+	var result []uintptr
+	var dfs func(addr uintptr)
+	dfs = func(addr uintptr) {
+		next := *(*uintptr)(unsafe.Pointer(addr))
+		if next == 0 {
+			return
+		}
+		if seen[next] {
+			return
+		}
+		result = append(result, next)
+		seen[next] = true
+		dfs(next)
+	}
+	for _, u := range roots {
+		result = append(result, u)
+		seen[u] = true
+		dfs(u)
+
+	}
+	return result
+}
